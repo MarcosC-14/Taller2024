e5eaa59f2b9a46e64c5b19d3b8cf8636a93cb382
MarcosC-14 <marcoscarab@hotmail.com>
2024-08-22 15:36:42 -0300

    Revert "Deleted useless folder"
    This reverts commit 5d9f1b45aec48089c49ebfd48099578ee078ddde.

---
+++ src/main/java/grupo7/taller/poo/Taller2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package grupo7.taller.poo;
+
+/**
+ *
+ * @author marco
+ */
+public class Taller2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
