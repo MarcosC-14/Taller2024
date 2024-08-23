ef6b8c8d5fc311ba24e2007b08f51e1b7cd95b9f
AngelinaMa123 <angiemavia@gmail.com>
2024-08-23 00:27:28 -0300

    Update Empleado Rol

---
+++ src/main/java/grupo7/taller/poo/modelo/Empleado.java
@@ -0,0 +1,34 @@
+
+package grupo7.taller.poo.modelo;
+
+
+public class Empleado extends Persona {
+    private Rol rol;
+
+    public Empleado() {
+        super();
+        rol= Rol.Cocinero;
+    }
+
+    public Empleado (String nombre, String correo, String contrasenia, Rol rol) {
+        super(nombre, correo, contrasenia);
+        this.rol = rol;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public void setRol(Rol rol) {
+        this.rol = rol;
+    }
+
+    @Override
+    public String toString() {
+        
+        return  "Empleado{nombre=" + getNombre() + ", correo=" + getCorreo() + ", contrasenia=" + getContrasenia() + " rol=" + rol + '}';
+    }
+    
+
+
+}
---
+++ src/main/java/grupo7/taller/poo/modelo/Rol.java
@@ -0,0 +1,8 @@
+
+package grupo7.taller.poo.modelo;
+
+
+public enum Rol {
+    Administrador, Recepcionista, Mesero, Maitre, Cocinero
+    
+}
