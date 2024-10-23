5018f3d8dbea7be0972ef2d22f70ad4707175d4b
AngelinaMa123 <angiemavia@gmail.com>
2024-10-22 22:26:20 -0300

    Documentacion clases Capacidad y Ubicacion

--- src/main/java/modelo/Capacidad.java
+++ src/main/java/modelo/Capacidad.java
@@ -1,7 +1,17 @@
 
 package modelo;
 
-
+/**
+* Esta enumeración representa la capacidad que tiene cada mesa.
+* Las capacidades posibles son:
+* dos: significa que una mesa tiene 2 sillas
+* cuatro: significa que una mesa tiene 4 sillas
+* seis: significa que una mesa tiene 6 sillas
+* ocho: significa que una mesa tiene 8 sillas
+* diez: significa que una mesa tiene 10 sillas
+* @author Marcos Ramon Caraballo, Angelina María Vialle,
+* @version 27/10/2024 
+*/
 public enum Capacidad {
     dos, cuatro, seis, ocho, diez
 }
--- src/main/java/modelo/Ubicacion.java
+++ src/main/java/modelo/Ubicacion.java
@@ -1,7 +1,14 @@
 
 package modelo;
 
-
+/**
+* Esta enumeración representa la ubicacion que tiene cada mesa.
+* Las ubicaciones posibles son:
+* Interior: significa que una mesa se ubica en el interior del restaurante.
+* Exterior: significa que una mesa se ubica en el exterior del restaurante.
+* @author Marcos Ramon Caraballo, Angelina María Vialle,
+* @version 27/10/2024 
+*/
 public enum Ubicacion {
     Interior, Exterior
 }
