b83ea822e23d3598359a3bb64c17e7f7c10a9c83
AngelinaMa123 <angiemavia@gmail.com>
2024-10-27 14:41:20 -0300

    Documentacion terminada ClienteController

--- src/main/java/Controladores/ClienteController.java
+++ src/main/java/Controladores/ClienteController.java
@@ -10,8 +10,6 @@ import java.awt.event.ActionListener;
 import Views.ClRegisterView;
 import Views.ClView;
 import java.awt.event.ActionEvent;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import javax.swing.JOptionPane;
 
 /**
@@ -19,9 +17,9 @@ import javax.swing.JOptionPane;
  * clientes, brindando metodos y controles de seguridad para que todos los datos
  * que este ingresen sean validos
  *
- * @author Marcos Ramon Caraballo, Angelina María Vialle,Valentin Rebechi,Ian
- * Caraballo
- * @version 27/10/2024
+ * @author Marcos Ramon Caraballo, Angelina María Vialle, Valentin Rebechi, Ian
+ * Caraballo.
+ * @version 27/10/2024.
  */
 public class ClienteController implements ActionListener {
 
@@ -31,7 +29,7 @@ public class ClienteController implements ActionListener {
     private ClView view2;
 
     /**
-     * *Constructor por defecto de la clase ClienteController.
+     * Constructor por defecto de la clase ClienteController.
      */
     public ClienteController() {
     }
@@ -40,8 +38,8 @@ public class ClienteController implements ActionListener {
      * Este metodo se encarga de registrar a un nuevo cliente en la base de
      * datos.
      *
-     * @param   cliente representa a un objeto cliente, el cual contendrá los
-     * datos a registrar.
+     * @param   cliente representa a un objeto de tipo cliente, el cual 
+     * contendrá los datos a registrar.
      * @param   clienteDAO contiene los metodos que interactuan entre cliente y
      * la base de datos.
      */
@@ -52,11 +50,11 @@ public class ClienteController implements ActionListener {
     }
 
     /**
-     *
-     * @param view hace referencia a la interfaz grafica que se le muestra al
-     * cliente al momento de registrarse. Tambien se pone el boton de confirmar
-     * en escucha para que cuando sea oprimido ejecute su metodo
-     * correspondiente.
+     * Guarda la referencia de la ventana ClRegisterView y permite que al 
+     * presionar en el boton Registrarse, se ejecute el metodo actionPerformed 
+     * de esta clase.
+     * @param   view representa a la ventana donde se registra el cliente 
+     * ClRegisterView
      *
      */
     public void setClRegisterView(ClRegisterView view) {
@@ -65,19 +63,15 @@ public class ClienteController implements ActionListener {
     }
 
     /**
-     * actionPerformed es el metodo que se ejecuta cuando se aprieta el boton de
-     * registrar
+     * Es el metodo que se ejecuta cuando se aprieta el boton Registrar.
+     * Controla que los datos ingresados sean validos, si es asi lo registra, si
+     * no muestra un mensaje pidiendo que complete los datos.
      *
      * @param e es el evento que activa el boton de registrar
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        /**
-         * En este if se controla que todos los datos ingresados dentro de los
-         * campos mostrados sean validos, si es asi confirma el registro, de lo
-         * contrario muestra una alerta solicitando que se completen todos los
-         * campos solicitados de forma adecuada.
-         */
+        
         if (e.getSource().equals(view.jButton_confirmarRegistroCliente)) {
             String nombre = view.jTextField_registroNombreCliente.getText();
             String correo = view.jTextField_correoRegistroCliente.getText();
@@ -109,10 +103,10 @@ public class ClienteController implements ActionListener {
     }
 
     /**
-     *
-     * @param correo es el correo ingresado por el usuario
-     * @return true si el correo ingresado contiene un @, de lo contrario
-     * retorna false
+     * Se encarga de revisar que el correo electronico tenga un @.
+     * @param   correo es el correo ingresado por el usuario.
+     * @return  true si el correo ingresado contiene un @, de lo contrario
+     * retorna false.
      */
     public static boolean esCorreoElectronicoValido(String correo) {
         return correo.contains("@");
