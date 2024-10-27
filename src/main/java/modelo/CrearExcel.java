4de1e2c3ee5ccd5483cd31afa5ceae350c7bb2fd
AngelinaMa123 <angiemavia@gmail.com>
2024-10-27 19:46:49 -0300

    Documentacion CrearExcel

--- src/main/java/modelo/CrearExcel.java
+++ src/main/java/modelo/CrearExcel.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
 import java.io.FileOutputStream;
@@ -16,15 +13,26 @@ import org.apache.poi.ss.usermodel.Row;
 import java.time.format.DateTimeFormatter;
 
 /**
- *
- * @author Marcos Ramon Caraballo, Angelina María Vialle, Valentin Rebechi, Ian
+ * La clase CrearExcel se encarga de crear los documentos tipo xlsx de:
+ * -Reserva.
+ * -Cliente.
+ * -Reservas por estacion.
+ * Ademas de disponer de un metodo para guardar cada archivo.
+ * @author  Marcos Ramon Caraballo, Angelina María Vialle, Valentin Rebechi, Ian
  * Caraballo.
  * @version 27/10/2024
  */
 public class CrearExcel {
 
     private static String FILE = "Excel.xlsx";
-
+/**
+ * Crea un archivo tipo xlsx de las Reservas, con un titulo y un ArrayList de 
+ * reservas.
+ * @param   titulo representa el nombre que tendra el documento.
+ * @param   reservas representa la lista de reservas que produciran el reporte.
+ * @throws  Exception puede generarla al crear el archivo o guadarla en la 
+ * carpeta.
+ */
     public static void hacerExcelReserva(String titulo, ArrayList<Reserva> reservas) throws Exception {
         FILE = "ExcelExportados/" + titulo + ".xlsx";
         Files.createDirectories(Paths.get("ExcelExportados"));
@@ -59,7 +67,14 @@ public class CrearExcel {
         }
         guardarArchivo(workBook, FILE);
     }
-
+/**
+ * Crea un archivo tipo xlsx de los clientes, con un titulo y un ArrayList de 
+ * clientes.
+ * @param   titulo representa el nombre que tendra el documento.
+ * @param   cliente representa la lista de clientes que produciran el reporte.
+ * @throws  Exception puede generarla al crear el archivo o guadarla en la 
+ * carpeta. 
+ */
     public static void hacerExcelCliente(String titulo, ArrayList<Cliente> cliente) throws Exception {
         FILE = "ExcelExportados/" + titulo + ".xlsx";
         Files.createDirectories(Paths.get("ExcelExportados"));
@@ -80,47 +95,47 @@ public class CrearExcel {
             row.createCell(0).setCellValue(cliente.get(i).getNombre());
 
             row.createCell(1).setCellValue(cliente.get(i).getTelefono());
-
             row.createCell(2).setCellValue(cliente.get(i).getCorreo());
-
         }
-
         guardarArchivo(workBook, FILE);
     }
-
+  
+/**
+ * Crea un archivo tipo xlsx de las reservas por estacion, con una cadena de 
+ * valores.
+ * @param   valores representa la cantidad de reservas por estacion que hubo.
+ * @throws  Exception puede generarla al crear el archivo o guadarla en la 
+ * carpeta.
+ */
     public static void HacerExcelEstaciones(String[] valores) throws Exception {
         FILE = "ExcelExportados/ConcurrenciasPorEstacion.xlsx";
         Files.createDirectories(Paths.get("ExcelExportados"));
         HSSFWorkbook workBook = new HSSFWorkbook();
-
         HSSFSheet Sheet = workBook.createSheet("Estacion");
-
         Row header = Sheet.createRow(0);
         header.createCell(0).setCellValue("Verano       ");
         header.createCell(1).setCellValue("Otoño       ");
         header.createCell(2).setCellValue("Invierno     ");
         header.createCell(3).setCellValue("Primavera     ");
-
         HSSFRow row = Sheet.createRow(1);
-
         row.createCell(0).setCellValue(valores[0]);
-
         row.createCell(1).setCellValue(valores[1]);
-
         row.createCell(2).setCellValue(valores[2]);
         row.createCell(3).setCellValue(valores[3]);
-
         guardarArchivo(workBook, FILE);
     }
-
+/**
+ * Se encarga de guardar el archivo.
+ * @param   workBook representa el archivo actual.
+ * @param   titulo representa el titulo que va a tener el archivo.
+ * @throws  IOException puede generar un error al cerrar el archivo.
+ */
     private static void guardarArchivo(HSSFWorkbook workBook, String titulo) throws IOException {
         try {
             workBook.write(new FileOutputStream(titulo));
         } catch (Exception e) {
-
         } finally {
             workBook.close();
         }
-
     }
 }
